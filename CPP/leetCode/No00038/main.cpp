a60b6df687905b1f7df1a71626c05dfe2350ff75
vvstorm <hyl19881203@sina.cn>
2020-06-13 23:29:15 -0700

    tmp

---
+++ CPP/leetCode/No00038/main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+    class Node {
+        public:
+        int nCount;
+        char cNumber;
+    }
+    string countAndSay(int n) {
+        vector<int> vec = {1};
+        for (auto index = 1; index <= n; ++index)
+        {
+
+        }
+    }
+};
