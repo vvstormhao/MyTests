97628bf820614f71758bd4df084f15e84680baa1
haoyunlong <haoyunlong@udesk.cn>
2020-06-22 20:41:55 +0800

    实现leetcode no803

--- CPP/leetCode/No00703/main.cpp
+++ CPP/leetCode/No00703/main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 class KthLargest {
 public:
-    KthLargest(int k, vector<int>& nums) {
-        
+    KthLargest(int k, vector<int>& nums) 
+    : nSize(k)
+    {
+        MakeHeap(nSize, nums);
+        SortK();
+        PrintTopK();
     }
     
     int add(int val) {
-
+        return 1;
     }
 private:
     void sink(vector<int>& nums, int nPos, int nSize)
@@ -45,14 +49,50 @@ private:
             sink(nums, i, nSize);
             i--;
         }
+
+        int nRange = min(int(nums.size()), k);
+
+        for (int j = 0; j < nRange; j++)
+        {
+            topK.push_back(nums[j]);
+        }
     }
 
-    void SortK(int k)
+    void SortK()
     {
+        vector<int> tmp;
+        int k = nSize;
+        while (k >= 0)
+        {
+            tmp.push_back(topK[k]);
+            swap(topK[0], topK[k]);
+            sink(topK, 0, k - 1);
+            --k;
+        }
 
+        topK = tmp;
+    }
+
+    void PrintTopK()
+    {
+        auto itBegin = topK.begin();
+        auto itEnd = topK.end();
+        for (; itBegin != itEnd; ++itBegin)
+        {
+            printf("%d ", *itBegin);
+        }
+        printf("\n");
     }
 private:
 
     vector<int> topK;
     int nSize;
 };
+
+int main()
+{
+    vector<int> vec = {3,7,5,1,15,90,45,6,23};
+    KthLargest *p = new KthLargest(5, vec);
+    delete p;
+    return 1;
+}
